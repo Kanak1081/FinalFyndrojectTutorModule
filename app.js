8327c6e866cc69219092d63fb7262e481f984898
Kanak <kanakyadav406@gmail.com>
2022-04-02 11:15:38 +0530

    changes in the UI of homepage

--- app.js
+++ app.js
@@ -35,14 +35,6 @@ app.use(morgan("dev"));
 
 app.use('/tutor',userRouter)
 
-//Handle production
-// if(process.env.NODE_ENV === 'production'){
-//     //Static folder
-//     app.use(express.static(__dirname + './public'))
-//     //handle SPA
-//     app.get(/.*/,(req,res)=> res.sendFile(__dirname + './public/index.html'))
-// }
-
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,'/front-end/dist')));
 
--- front-end/src/components/GettingStarted.vue
+++ front-end/src/components/GettingStarted.vue
@@ -1,40 +1,35 @@
 <template>
 <!-- eslint-disable -->
- <div class="container-fluid">
-     <div class="row">
-    <div class="col-sm-4 col-md-4 col-lg-4"></div>
-    <div class="col-sm-4 col-md-4 col-md-4">
-        <img class="h-75" :src="logoFynd">
-    </div> 
-    <div class="col-sm-4 col-md-4 col-lg-4"></div>
-</div>
-
-<div class="row" >
-    <div class="col-md-1 col-lg-1"></div>
-    <div class="col-xs-8 col-sm-8 col-md-6 col-lg-4">
-        <h1 class="fs-1 text-start fw-bold">
-            <span id="closerID" class="d-block">One step closer to</span>
-            <span id="empower" class="d-block">empower engineers </span>
-        </h1>
-        <p id="fyndDescription" class="fs-5 text-start fw-bold">
-            Fynd Academy sets up engineers on a lifelong growth path. Learn what our Trainee dream and work towards, and prepare to start your programming career with the best.
-        </p>
-        <div class="text-start">
-            <a  href="/sign-up" class="btn btn-success btn-lg btn-block" style="margin-right:2%">
-                Register
-            </a>
-            <a  href="/login" class="btn btn-success btn-lg btn-block">
-                Login
-            </a>
-        </div>
-    </div> 
-    <div class="col-md-1 col-lg-2"></div>
-    <div class="ccol-xs-12 col-sm-12 col-md-3 col-lg-4">
-        <img class="h-100" src="../assets/fyndAcademy.png" alt="" />
+<div class="container-fluid" id="title">
+    <div class="row">
+        <div class="col-lg-12">
+            <img class="h-75" :src="logoFynd">
+        </div> 
     </div>
-    <div class="col-md-1 col-lg-1"></div>
+
+    <div class="row" >
+        <div class="col-lg-7" id="title_description">
+            <h1 class="fw-bold">
+                <span id="closerID" class="d-block">One step closer to</span>
+                <span id="empower" class="d-block">empower engineers </span>
+            </h1>
+            <p id="fyndDescription" class="fw-bold mt-3">
+                Fynd Academy sets up engineers on a lifelong growth path. Learn what our Trainee dream and work towards, and prepare to start your programming career with the best.
+            </p>
+            <div class="text-start mt-3">
+                <a  href="/sign-up" class="btn btn-success btn-lg btn-block" style="margin-right:2%">
+                    Register
+                </a>
+                <a  href="/login" class="btn btn-success btn-lg btn-block">
+                    Login
+                </a>
+            </div>
+        </div> 
+        <div class="col-lg-5">
+            <img class="d-none d-sm-block d-sm-none d-md-block" src="../assets/fyndAcademy.png" alt="" />
+        </div>
+    </div>
 </div>
- </div>
 
 
 </template>
@@ -56,21 +51,33 @@ beforeCreate () {
 }
 </script>
 
-<style>
+<style scoped>
+.container-fluid{
+  padding: 0% 15%;
+}
+h1{
+    font-size: 3rem;
+    text-align: left;
+    font-family: "Montserrat-Bold";
+}
 body{
     overflow-x:hidden;
 }
 #empower{
     color:#da388d;
 }
-/* #getStartedBtn{
-    background:#da388d;
-    color:black;
-} */
+
 #fyndDescription{
     color:#069ba2;
+    text-align: left;
+    font-family: "Montserrat";
+    font-size: 1rem;
 }
 #closerID{
     color:#069ba2;
 }
+#title{
+    text-align: center;
+}
+
 </style>
